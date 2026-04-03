05a9f1347eaa5a4288e37ad067812a300acbfb2c
fideliokim <rlaalsrb720@gmail.com>
2026-04-03 13:47:10 +0900

    [refactoring] 51p

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -4,6 +4,10 @@ public class Game {
 
 
     public void guess(String guessNumber) {
+        assertIllegalArgument(guessNumber);
+    }
+
+    private void assertIllegalArgument(String guessNumber) {
         if(guessNumber == null){
             throw new IllegalArgumentException();
         }
@@ -18,10 +22,14 @@ public class Game {
             }
         }
 
-        if(guessNumber.charAt(0) == guessNumber.charAt(1)
-            || guessNumber.charAt(0) == guessNumber.charAt(2)
-        || guessNumber.charAt(1) == guessNumber.charAt(2)){
+        if(isDuplicatedNumber(guessNumber)){
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isDuplicatedNumber(String guessNumber) {
+        return guessNumber.charAt(0) == guessNumber.charAt(1)
+                || guessNumber.charAt(0) == guessNumber.charAt(2)
+                || guessNumber.charAt(1) == guessNumber.charAt(2);
     }
 }
